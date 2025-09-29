11724fa066993d4de2ed9111c9512c45a0a2fd2a
Anisa Berjani <anisa.berjani@gmail.com>
2025-09-29 10:53:57 +0200

    Styling chat message bubbles

--- lib/widgets/chat_messages.dart
+++ lib/widgets/chat_messages.dart
@@ -1,4 +1,6 @@
+import 'package:chats/widgets/message_bubble.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessages extends StatelessWidget {
@@ -6,26 +8,57 @@ class ChatMessages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final authenticatedUser = FirebaseAuth.instance.currentUser!;
+
     return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('chat').orderBy('createdAt',descending: false).snapshots(),
-        builder: (ctx, chatSnapshots){
-          if(chatSnapshots.connectionState == ConnectionState.waiting){
-            return const Center(child: CircularProgressIndicator(),);
-          }
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: true)
+          .snapshots(),
+      builder: (ctx, chatSnapshots) {
+        if (chatSnapshots.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
 
-          if(!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty){
-            return const Center(child: Text('No meesages found.'),);
-          }
+        if (!chatSnapshots.hasData || chatSnapshots.data!.docs.isEmpty) {
+          return const Center(child: Text('No meesages found.'));
+        }
 
-          if(chatSnapshots.hasError){
-            return const Center(child: Text('Something went wrong...'),);
-          }
+        if (chatSnapshots.hasError) {
+          return const Center(child: Text('Something went wrong...'));
+        }
 
-          final loadedMessages = chatSnapshots.data!.docs;
-          return ListView.builder(itemCount: loadedMessages.length,itemBuilder: (ctx, index) =>
-              Text(loadedMessages[index].data()['text'])
-          );
-        },
+        final loadedMessages = chatSnapshots.data!.docs;
+        return ListView.builder(
+          padding: const EdgeInsets.only(bottom: 40, left: 13, right: 13),
+          reverse: true,
+          itemCount: loadedMessages.length,
+          itemBuilder: (ctx, index) {
+            final chatMessage = loadedMessages[index].data();
+            final nextChatMessage = index + 1 < loadedMessages.length
+                ? loadedMessages[index + 1].data()
+                : null;
+
+            final currentMessageUserId = chatMessage['userId'];
+            final nextMessageUserId = nextChatMessage != null
+                ? nextChatMessage['userId']
+                : null;
+            final nextUserIsSame = nextMessageUserId == currentMessageUserId;
+
+            if (nextUserIsSame) {
+              return MessageBubble.next(message: chatMessage['text'],
+                  isMe: authenticatedUser.uid == currentMessageUserId);
+            } else {
+              return MessageBubble.first(
+                  userImage: chatMessage['userImage'],
+                  username: chatMessage['username'],
+                  message: chatMessage['text'],
+                  isMe: authenticatedUser.uid == currentMessageUserId,
+              );
+            }
+          },
+        );
+      },
     );
   }
 }
